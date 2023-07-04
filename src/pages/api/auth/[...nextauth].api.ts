de74dba28bd38167738ae320b6dbd2cfc01eb4cf
lucasabdouni <lucasabdouni@hotmail.com>
2023-07-04 13:24:48 -0300

    add and configure next-auth for google

---
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -0,0 +1,42 @@
+import { PrismaAdapter } from '@/src/lib/auth/prisma-adapter'
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import GoogleProvider from 'next-auth/providers/google'
+
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope:
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+          },
+        },
+      }),
+    ],
+
+    callbacks: {
+      async signIn({ account }) {
+        if (
+          !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+        ) {
+          return '/register/connect-calendar/?error=permissions'
+        }
+
+        return true
+      },
+    },
+  }
+}
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
