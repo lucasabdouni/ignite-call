6f60e91a09efecd8e53e1a7422d2d20f71164e24
lucasabdouni <lucasabdouni@hotmail.com>
2023-08-31 18:54:49 -0300

    add and configure next-seo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,29 +4,37 @@ import Image from 'next/image'
 
 import previewImagem from '../../assets/app-preview.png'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
 
-        <ClaimUsernameForm />
-      </Hero>
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
 
-      <Preview>
-        <Image
-          src={previewImagem}
-          alt="Calendario simbolizando aplicação em funcionamento"
-          height={400}
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImagem}
+            alt="Calendario simbolizando aplicação em funcionamento"
+            height={400}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
